447d3e5e398af43be29cb77fdac6c02b4be7a945
unknown <raheenakp01@gmail.com>
2025-06-20 08:21:04 +0300

    updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/')
+@app.route('/home')
 def home(): return render_template('index.html')
 @app.route('/menu')
 def menu(): return render_template('menu.html')
